1b2257e35cf91053af5044b1c48b3fd1bfd85e78
Ишутинов Данил <gladius@inbox.ru>
2023-06-25 18:23:43 +0500

    18-23

--- IDA_HomeWork_OOP_7 Coat+Flat/Contain_and_Handle.h
+++ IDA_HomeWork_OOP_7 Coat+Flat/Contain_and_Handle.h
@@ -15,6 +15,86 @@ class Contain_and_Handle
 	static std::vector <OverCoat*> _objects_list;
 	static std::map <int, std::string> codes_of_operation;
 public:
+	
+	static void Initialisation(int random_number)
+	{
+
+		for (int i = 0; i < random_number; i++)
+		{
+			OverCoat* new_item = new OverCoat('rand');		
+		}
+
+	}
+
+
+	// Shows ---------------------------------------------------------------	
+	static void ShowAvailableObjects();
+
+
+	// Inputs handle -------------------------------------------------------	
+
+	static void Mode_Input_Handle()
+	{
+		std::cout << "\nChoose mode :";
+		std::cout << "\n1. SELF test mode";
+		std::cout << "\n2. Interactive mode\n";
+
+		switch (_getch_to_int(1, 2))
+		{
+		case -1: return;
+		case 1: UnitTest::OverCoat_testing(); break;
+		case 2: UserChoiceHandle_getch; break;
+		}
+
+		//return _getch_to_int(1,2);
+	}
+	static int _getch_to_int(int minimum=0, int maximum=9)
+	{	
+			int keycode = _getch();
+			if (keycode == 27) return -1;			
+			if ((keycode >= 48 +minimum) && (keycode <= 48+ maximum))
+				return (keycode - 48);
+			//std::cout << "\nkeycode " << keycode;
+			std::cout << "Press a number [" << minimum << ".." << maximum << "] ";
+			_getch_to_int(minimum, maximum);		
+	}
+
+
+// getch модификация - удобный интерфейс ввода без нажатия Enter и с обработкой Esc, пока только для списков 0..9 (надо придумать как принимать одно- и двух-циферные значения в одном интерфейсе через _getch
+// getline модификация - c вводом произвольных чисел и конторолем ввода через функцию Get_Int_Positive(), ввод необходимо подтверждать Enter
+
+//for (;;)
+//{
+//	cout << "You are playing for:" << playtime << "seconds." << endl;
+//	cout << "You have " << bytes << " bytes." << endl;
+//	cout << "You are compiling " << bps << " bytes per second." << endl;
+//	cout << "Press a to buy assembler monkey (produces 1 byte per second)/(cost 10 bytes)" << endl;
+//	if (kbhit()) {  //is true when a key was pressed
+//		char c = getch();   //capture the key code and insert into c
+
+//		switch (c)
+//		{
+//		case 'a': bytes = bytes - 10; bps++; break;
+//		}
+//	}
+//	bytes = bytes + bps;
+//	playtime++;
+//	Sleep(1000);
+//	system("cls");
+//}
+
+	static int UserChoiceHandle_getch();
+	static int UserChoiceHandle_getline();
+
+	static OverCoat* Get_Operand_getch(int Operand_number);
+	static OverCoat& Get_Operand_getline(int Operand_number);
+	static OverCoat& New_money_holder_input();
+
+	static int Check_method_input_getch();
+	static int Check_action_input_getline();
+
+	static void Operation_module(int action, OverCoat& Operand_1, OverCoat& Operand_2);
+	static void Operation_module(int action, OverCoat& Operand_1, double Operand_2);
 
 };
 
--- IDA_HomeWork_OOP_7 Coat+Flat/OverCoat.h
+++ IDA_HomeWork_OOP_7 Coat+Flat/OverCoat.h
@@ -8,76 +8,62 @@
 #include <iomanip>
 #include <cassert>
 
+enum class Manufacter {BOSS, ZARA, HandM, noname, adidas, terminator};
+enum class Material_type { Fur, Leather, Textile, Syntetic, terminator};
+enum class OverCoat_colors {black, brown, yellow, white, purple, indigo, terminator};
+enum Overcoat_length_type {top, jacket, manto, terminator};
+enum Dress_size { kid, XXS, XS, S, M, L, XL, XXL, terminator};
+std::map<int, std::string> Manufacter_codename{ {0, "BOSS"} ,{1, "ZARA"},{2, "HandM"}, {3, "noname"}, {4, "adidas"} };
+
 class OverCoat
 {
-	std::string _name;
-	enum type { Coat, Fur };
-	int _size;	
+	Overcoat_length_type _length;
+	Manufacter _manufacter;
+	OverCoat_colors _color;
+	Material_type _material_type;
+	Dress_size _size;
+	std::string _comment;
+	double _price; //rub
 public:
 	// Constructors // Initialisation // Destructors // Memory Clean -------
-	OverCoat(long long coins = 0, long long roubles = 0);
-	//OverCoat() {}
+	OverCoat(std::string comment, Dress_size size, Material_type overcoat_material_type,	OverCoat_colors color, Overcoat_length_type length, double _price);
+	OverCoat(char random_flag) 
+	{
+		//OverCoat* new_item = new OverCoat;
+		_size = static_cast<Dress_size>(Get_Random(0, int(Dress_size::terminator)));
+		_material_type = static_cast<Material_type>(Get_Random(0, int(Material_type::terminator)));
+		_color = static_cast<OverCoat_colors>(Get_Random(0, int(OverCoat_colors::terminator)));
+		_length = static_cast<Overcoat_length_type>(Get_Random(0, int(Overcoat_length_type::terminator)));
+		_manufacter = static_cast<Manufacter>(Get_Random(0, int(Manufacter::terminator)));
+	}
+
 	~OverCoat()
 	{
+		//_manufacter
+
+		//OverCoat_colors->size();
+		//_color = OverCoat_colors::black;		
+
 		//std::cout << "Vector reallocates memory";
 		//_getch();
 	}
-	static void Initialisation(int random_number)
-	{
-		for (int i = 0; i < random_number; i++)
-			new OverCoat();
-	}
 	static void Memory_Clean();
 
+	static void Get_methods()
+	{
+
+	}
+
 	// Setters // Getters --------------------------------------------------
-	OverCoat& Set_roubles(long long rubles);
-	OverCoat& Set_coins(long long coins);
-	long long Get_roubles() { return _roubles; }
-	int Get_coins() { return _coins; }
+	OverCoat& Set_comment(std::string comment) { _comment = comment; return *this; }
+	OverCoat& Set_price(double price) { _price = price; return *this; }
+
 
 	// Shows ---------------------------------------------------------------
 	void ShowValues() { std::cout << *this; }
 	//void ShowValues() { std::cout << this; }
+
 	static void ShowMethods();
-	static void ShowAvailableMoneyObjects();
-
-	// Inputs handle -------------------------------------------------------	
-		
-	// getch модификация - удобный интерфейс ввода без нажатия Enter и с обработкой Esc, пока только для списков 0..9 (надо придумать как принимать одно- и двух-циферные значения в одном интерфейсе через _getch
-	// getline модификация - c вводом произвольных чисел и конторолем ввода через функцию Get_Int_Positive(), ввод необходимо подтверждать Enter
-
-	//for (;;)
-	//{
-	//	cout << "You are playing for:" << playtime << "seconds." << endl;
-	//	cout << "You have " << bytes << " bytes." << endl;
-	//	cout << "You are compiling " << bps << " bytes per second." << endl;
-	//	cout << "Press a to buy assembler monkey (produces 1 byte per second)/(cost 10 bytes)" << endl;
-	//	if (kbhit()) {  //is true when a key was pressed
-	//		char c = getch();   //capture the key code and insert into c
-
-	//		switch (c)
-	//		{
-	//		case 'a': bytes = bytes - 10; bps++; break;
-	//		}
-	//	}
-	//	bytes = bytes + bps;
-	//	playtime++;
-	//	Sleep(1000);
-	//	system("cls");
-	//}
-
-	static int UserChoiceHandle_getch();
-	static int UserChoiceHandle_getline();
-
-	static OverCoat* Get_Operand_getch(int Operand_number);
-	static OverCoat& Get_Operand_getline(int Operand_number);
-	static OverCoat& New_money_holder_input();
-
-	static int Check_method_input_getch();
-	static int Check_action_input_getline();
-
-	static void Operation_module(int action, OverCoat& Operand_1, OverCoat& Operand_2);
-	static void Operation_module(int action, OverCoat& Operand_1, double Operand_2);
 
 	//Overload operators - reference returned -----------------------------------------
 
@@ -93,5 +79,11 @@ public:
 	bool operator==(const OverCoat& another_Money) const;
 
 	friend std::ostream& operator << (std::ostream& out, OverCoat* just_a_Money);
-	friend std::ostream& operator << (std::ostream& out, OverCoat& Money_holder);
+	friend std::ostream& operator << (std::ostream& out, OverCoat& OverCoat_obj)
+	{
+		std::cout << std::endl << OverCoat_obj._length;
+		std::cout << std::endl << Manufacter_codename[int(OverCoat_obj._manufacter)];
+
+	}
 };
+
